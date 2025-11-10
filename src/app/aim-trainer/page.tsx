ce6e12a05960bbb45c42984455d0c82c2bad4567
Sanjay Explain <sanjayexplain@gmail.com>
2025-11-10 17:17:39 +0000

    Iss button ko thoda bada karo upar ki site (_for element <Button>_)

--- src/app/aim-trainer/page.tsx
+++ src/app/aim-trainer/page.tsx
@@ -221,7 +221,7 @@ export default function AimTrainerPage() {
                       <div className="text-xs uppercase">Accuracy</div>
                     </div>
                   </div>
-                  <Button onClick={() => setGameState('idle')} size="lg">
+                  <Button onClick={() => setGameState('idle')} size="lg" className="mt-4">
                     <History className="mr-2 h-5 w-5" />
                     Play Again
                   </Button>
